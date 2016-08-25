6d99bac826bdbf3f6f3aec7f01235b649b643d30
Impuls3iv <sammy.unterman@gmail.com>
2016-08-24 20:30:02 -0400

    working on persisting data to backend from js, and did some styling

--- app/assets/javascripts/cupboards.js
+++ app/assets/javascripts/cupboards.js
@@ -1,5 +1,13 @@
 $(function(){
   console.log('cupboard js loaded')
 
-  
+  // color all the circles
+  $('#clr1').css("background-color", "#FFFFFF");
+  $('#clr2').css("background-color", "#804002");
+  $('#clr3').css("background-color", "#808080");
+  $('#clr4').css("background-color", "#000000");
+  $('#clr5').css("background-color", "#EADAC1");
+  $('#clr6').css("background-color", "#F7EEE5");
+  $('#clr7').css("background-color", "#715F39");
+  $('#clr8').css("background-color", "#02247D");
 });
