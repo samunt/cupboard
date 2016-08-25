e30bfcc4ec6852ad36bead620b90670a15c69fa8
Impuls3iv <sammy.unterman@gmail.com>
2016-08-24 20:41:32 -0400

    going to test first ajax

--- app/assets/javascripts/cupboards.js
+++ app/assets/javascripts/cupboards.js
@@ -1,6 +1,6 @@
 $(function(){
   console.log('cupboard js loaded')
-
+  var color = "none"
   // color all the circles
   $('#clr1').css("background-color", "#FFFFFF");
   $('#clr2').css("background-color", "#804002");
@@ -10,4 +10,15 @@ $(function(){
   $('#clr6').css("background-color", "#F7EEE5");
   $('#clr7').css("background-color", "#715F39");
   $('#clr8').css("background-color", "#02247D");
+
+  //persist data to back end with ajax on click
+  $('#clr1').on('click', function(){
+    console.log('color 1 clicked')
+    color = "#FFFFFF"
+    $.ajax{
+      url: "/users/:id/cupboards/:id/update",
+      method: "POST",
+      data: { color }
+    }
+  });
 });
